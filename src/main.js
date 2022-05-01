4ab3edc6f228d98882c385c1017d22a925f87f67
ilyaleex <gooooodguy@mail.ru>
2022-05-01 14:59:53 +0300

    changed popup rendering place

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {render} from './render.js';
+import {render, RenderPosition} from './render.js';
 import ProfileRatingView from './view/profile-rating-view.js';
 import NavigationView from './view/navigation-view.js';
 import MovieAmountView from './view/movie-amount-view.js';
@@ -10,13 +10,14 @@ import PopupFilmDetailsView from './view/popup-film-details-view.js';
 const body = document.querySelector('body');
 const header = body.querySelector('.header');
 const main = body.querySelector('.main');
-const footerStatistics = body.querySelector('.footer__statistics');
+const footer = body.querySelector('.footer');
+const footerStatistics = footer.querySelector('.footer__statistics');
 const filmsContainerPresenter = new FilmsContainerPresenter();
 
 render(new ProfileRatingView(), header);
 render(new NavigationView(), main);
 render(new SortingView(), main);
 render(new MovieAmountView(), footerStatistics);
-render(new PopupFilmDetailsView(), body);
+render(new PopupFilmDetailsView(), footer, RenderPosition.AFTEREND);
 
 filmsContainerPresenter.init(main);
