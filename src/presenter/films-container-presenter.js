c42be28519a3da369651a6730aeb549c4158ecaa
ilyaleex <gooooodguy@mail.ru>
2022-06-19 23:24:37 +0300

    profile rating fix

--- src/presenter/films-container-presenter.js
+++ src/presenter/films-container-presenter.js
@@ -147,7 +147,6 @@ export default class FilmsContainerPresenter {
         this.#updateFilmData(data);
         break;
       case UpdateType.MAJOR:
-        this.#renderProfile();
         this.#clearContainer({resetRenderedFilmsCount: true});
         this.#renderContainer();
         break;
--- src/view/profile-rating-view.js
+++ src/view/profile-rating-view.js
@@ -2,8 +2,8 @@ import AbstractView from '../framework/view/abstract-view.js';
 import {getProfileRating} from '../utils/profile.js';
 
 const createProfileRatingTemplate = (films) => (
-  `<section class="header__profile profile">
-    <p class="profile__rating ${getProfileRating(films) ? '' : 'visually-hidden'}">${getProfileRating(films)}</p>
+  `<section class="header__profile profile ${getProfileRating(films) ? '' : 'visually-hidden'}">
+    <p class="profile__rating">${getProfileRating(films)}</p>
     <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
   </section>`
 );
